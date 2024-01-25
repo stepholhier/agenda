2ab11d4ab45ff70b0dcf7d1d9abac982f928b4b5
Stephano <stegoodds@gmail.com>
2024-01-25 08:45:56 -0300

    Inserção cód

---
+++ agenda.py
@@ -0,0 +1,43 @@
+def adicionar_contato(nome, email, telefone):
+    dados = {"nome": nome, "email": email, "telefone": telefone, "completada": False}
+    lista.append(dados)
+    print(f"Informações do contato: Nome: {nome}, E-mail: {email}, Telefone: {telefone}")
+    return print ("Contato adicionado com sucesso")
+
+
+def visualizar_contato(lista):
+    print("\nLista de Contatos: ")
+    for indice, dados in enumerate(lista, start=1):
+        status = "❤️" if dados["completada"] else " "
+        contato = dados["nome"]
+        print(f"{indice}. [{status}] {contato}")
+
+#Adicionando editar contato
+# def editar_contato(lista):
+
+lista = []
+while True:
+    print("1. Adicionar contato: ")
+    print("2. Visualizar contatos cadastrados: ")
+    print("3. Editar contato: ")
+    print("4. Adicionar contato aos favoritos: ")
+    print("5. Ver lista de favoritos: ")
+    print("5. Excluir contato: ")
+    print("6. Sair")
+
+
+    escolha = input("Digite sua escolha: ")
+    
+    if escolha == "1":
+        nome = input("Digite o nome do contato: ")
+        email = input("Digite o seu e-mail: ")
+        telefone = input("Digite seu número de telefone: ")
+        adicionar_contato(nome, email, telefone)
+
+    elif escolha == "2":
+        visualizar_contato(lista)
+    
+    elif escolha == "6":
+        break
+
+print("Agenda finalizada com sucesso")
